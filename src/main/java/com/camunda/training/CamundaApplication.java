2c950787b21fdbc9e06d311c72569dd8a36c4580
Jonathan Lukas <81753891+jonathanlukas@users.noreply.github.com>
2022-10-10 10:56:30 +0200

    Update CamundaApplication.java

--- src/main/java/com/camunda/training/CamundaApplication.java
+++ src/main/java/com/camunda/training/CamundaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProcessApplication("twitter-qa")
+@EnableProcessApplication("webshop")
 public class CamundaApplication {
   public static void main(String... args) {
     SpringApplication.run(CamundaApplication.class, args);
