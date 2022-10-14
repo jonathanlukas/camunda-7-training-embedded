7c5c7c916ea77cf1e19e6cb750ee2628bd18671b
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-14 09:45:30 +0200

    ex3.6

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,14 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.jupiter.api.Test;
+
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import static org.assertj.core.api.Assertions.*;
 public class ProcessJUnitTest {
   
-//  @Test
+  @Test
+  @Deployment(resources = "payment_process.bpmn")
   public void testHappyPath() {
     
   }
