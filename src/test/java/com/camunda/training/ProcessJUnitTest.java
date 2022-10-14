bfebe07efbe89501f076978498dd3ec4cc164939
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-14 09:46:36 +0200

    ex3.8

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,9 +1,13 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import static org.assertj.core.api.Assertions.*;
@@ -14,7 +18,14 @@ public class ProcessJUnitTest {
   @Test
   @Deployment(resources = "payment_process.bpmn")
   public void testHappyPath() {
-    
+    // Create a HashMap to put in variables for the process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("orderTotal", 30.00);
+    variables.put("customerCredit", 20.00);
+    // Start process with Java API and variables
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("PaymentProcess", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance).isEnded().hasPassed("Activity_Charge_Credit_Card");
   }
 
 }
