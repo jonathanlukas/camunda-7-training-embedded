fdca9b0fd1f7c7f3759d84623080ab2298d376b4
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-14 09:46:01 +0200

    ex3.7

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,14 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(ProcessEngineCoverageExtension.class)
 public class ProcessJUnitTest {
   
   @Test
