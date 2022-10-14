019f9ad6174b6d4f9d223ec0dac614ff850acb49
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-14 09:44:46 +0200

    ex3.5

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,5 +1,7 @@
 package com.camunda.training;
 
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.assertj.core.api.Assertions.*;
 public class ProcessJUnitTest {
   
 //  @Test
